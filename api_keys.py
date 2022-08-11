99d01ad23f5d23ddb5237d1b30369d39d219a186
orjadamucollari <orjadamucollari@gmail.com>
2022-08-11 15:31:53 +0200

    first commit

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+"""
+Main keys for api connections
+"""
+# Key for openweather API (https://openweathermap.org/)
+OWM_key = " "
+# Key  for aerodatabox API from x-rapid (https://rapidapi.com/aedbx-aedbx/api/aerodatabox/)
+flight_api_key = " "
+
